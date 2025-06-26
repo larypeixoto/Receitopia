e5d081b71f35eeacc17c68258086fb35ed8fb317
Rodrigo Schuab <rodschuab@gmail.com>
2025-06-25 21:06:32 -0300

    adicionando botao

---
+++ Receitopia/src/components/Button/styles.ts
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  button: {
+    backgroundColor: "#7D8F69",
+    paddingVertical: 12,
+    paddingHorizontal: 30,
+    borderRadius: 20,
+    marginHorizontal: 5,
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "#FFF",
+    fontSize: 16,
+  },
+});
