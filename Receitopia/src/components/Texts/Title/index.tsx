f0853717cd0d7c98e340f5efe3574d63b5c3065e
Beatriz Pinheiro <beatrizpinheiro2001@gmail.com>
2025-06-27 00:01:37 -0300

    adicionando fontes

--- Receitopia/src/components/Texts/Title/index.tsx
+++ Receitopia/src/components/Texts/Title/index.tsx
@@ -1,11 +1,19 @@
 import { Text } from "react-native";
 import { styles } from "./styles";
+import { useFonts, Tangerine_700Bold } from '@expo-google-fonts/tangerine';
+
 
 interface TitleProps {
     title?: string;
 }
 
 export const Title = ({title}: TitleProps) => {
+    const [fontsLoaded] = useFonts({
+     Tangerine_700Bold,
+});
+
+if (!fontsLoaded) return null;
+
     return(
         <>
         <Text style={styles.titleFont}>
