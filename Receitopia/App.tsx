1827962f92a1b64aaf93e7415e3f6107d9c8a564
Enzo Dias <lenzo7085@gmail.com>
2025-06-26 22:46:17 -0300

    cadastro de cliente funcionando

--- Receitopia/App.tsx
+++ Receitopia/App.tsx
@@ -1,9 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 
 import { Routes } from "./src/routes/routes"
+import { Register } from './src/screens/Register';
 
 export default function App() {
   return (
-    <Routes/>
+    <Register/>
   );
 }
---
+++ Receitopia/src/services/api.ts
@@ -0,0 +1,7 @@
+import axios from 'axios'
+
+export const apiMock = axios.create({
+    baseURL: 'https://685db4087b57aebd2af6e8a0.mockapi.io'
+});
+
+ 
