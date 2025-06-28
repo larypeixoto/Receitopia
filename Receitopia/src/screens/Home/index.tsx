af117bc849502d10863fc3ab3e3cea72d02c2a10
Laryssa Peixoto <laryssa.peixot@outlook.com>
2025-06-27 21:59:16 -0300

    Correção home

--- Receitopia/src/screens/Home/index.tsx
+++ Receitopia/src/screens/Home/index.tsx
@@ -4,7 +4,7 @@ import {
   Image,
   ScrollView,
   ActivityIndicator,
-FlatList,
+  FlatList,
 } from "react-native";
 
 import { useEffect, useState } from "react";
@@ -12,11 +12,10 @@ import { getRecipes, recipeProps } from "../../services/recipesApi";
 import { Background } from "../../components/Background";
 import { RecipeCard } from "../../components/Card/RecipeCard";
 import { Title } from "../../components/Texts/Title";
-import { Separator } from "../../components/Separator"
+import { Separator } from "../../components/Separator";
 import { NavigationContainer } from "@react-navigation/native";
 import { BottomTabs } from "../../routes/BottomTabs";
-import { styles } from "./styles"
-
+import { styles } from "./styles";
 
 export const HomePage = () => {
   const [recipeList, setRecipeList] = useState<recipeProps[]>([]);
@@ -36,27 +35,27 @@ export const HomePage = () => {
   return (
     <>
       <Background>
-        <ScrollView>
+        <View style={styles.header}>
+          <Title title={"Receitopia"} />
+        </View>
 
-          <View style={styles.header}>
-              <Title title={"Receitopia"}/>
-          </View>
-
-          {loading ? (
-            <ActivityIndicator size={"large"} />
-          ) : (
-            <FlatList
-              data={recipeList}
-              renderItem={({ item }) => {
-                return <RecipeCard receita={item.receita}
-                id={item.id}
-                tipo={item.tipo}
-                link_imagem={item.link_imagem} />;
-              }}
-            />
-          )}
-        </ScrollView>
-        
+        {loading ? (
+          <ActivityIndicator size={"large"} />
+        ) : (
+          <FlatList
+            data={recipeList}
+            renderItem={({ item }) => {
+              return (
+                <RecipeCard
+                  receita={item.receita}
+                  id={item.id}
+                  tipo={item.tipo}
+                  link_imagem={item.link_imagem}
+                />
+              );
+            }}
+          />
+        )}
       </Background>
     </>
   );
