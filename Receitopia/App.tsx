3bc035676f9299eba76afdd8376f0a402a6709fa
Enzo Dias <lenzo7085@gmail.com>
2025-07-02 14:05:15 -0300

    colocando context

--- Receitopia/App.tsx
+++ Receitopia/App.tsx
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-
+import { NotificacaoProvider } from "./src/contexts/Notificação";
 import { Routes } from "./src/routes/routes"
 import { Register } from './src/screens/Register';
 import { SingIn } from './src/screens/SingIn';
 
 export default function App() {
   return (
-    <Routes/>
+    <NotificacaoProvider>
+      <Routes/>
+    </NotificacaoProvider>
+    
     
   );
 }
